bdd160b3e3771858d2d812a2fdaedb95ea6c2c67
Miguel Nicolas <miguelqnicolas@gmail.com>
2020-01-16 02:16:49 -0500

    met mvp

--- api/server.js
+++ api/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
+const cors = require('cors');
+const postRouter = require('../posts/postRouter');
+const userRouter = require('../users/userRouter');
 
 const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use('/api/users', logger, userRouter);
 
 server.get('/', (request, response) => {
   	response.send({message: 'router is working'});
 });
 
 // custom middleware
-function logger(req, res, next) {
-
+function logger(request, response, next) {
+	console.log(request.method, 'to', request.originalUrl, request.body, 'at', Date.now());
+	next();
 };
 
 module.exports = server;
